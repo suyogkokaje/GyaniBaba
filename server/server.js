7280061255249e2fa0aed82f2f812906e4628973
suyogkokaje <suyogkokaje6@gmail.com>
2022-12-24 08:46:58 +0530

    server code

---
+++ server/server.js
@@ -0,0 +1,51 @@
+import express from 'express';
+import * as dotenv from 'dotenv';
+import cors from 'cors';
+import { Configuration, OpenAIApi } from 'openai';
+
+dotenv.config();//access to .env file
+
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);//instance of OpenAI
+
+const app = express();//initialize express app
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+//routes
+app.get('/', async (req, res) => {
+    res.status(200).send({
+        message: 'Namaste from Gyani Baba',
+    })
+});
+
+app.post('/', async (req, res) => {
+    try {
+        const prompt = req.body.prompt;
+        const response = await openai.createCompletion({
+            model: "text-davinci-003",
+            prompt: `${prompt}`,
+            temperature: 0,//setting temperatur to 0 as we want to get the exact answer and not any randm answer
+            max_tokens: 3000,
+            top_p: 1,
+            frequency_penalty: 0.5,
+            presence_penalty: 0,
+        });
+        res.status(200).send({
+            bot: response.data.choices[0].text,
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(500).send({err});
+    }
+});
+
+//listen
+app.listen(5000, () => {
+    console.log('Server is running on http://localhost:5000');
+});
