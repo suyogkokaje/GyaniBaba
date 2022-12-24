6a6522e629d192afc81ca2cf41239a18a952a42f
suyogkokaje <suyogkokaje6@gmail.com>
2022-12-24 11:37:22 +0530

    final commit (maybe)

--- server/server.js
+++ server/server.js
@@ -41,7 +41,7 @@ app.post('/', async (req, res) => {
         });
     } catch (err) {
         console.log(err);
-        res.status(500).send(error || 'Something went wrong');
+        res.status(500).send(err || 'Something went wrong');
     }
 });
 
